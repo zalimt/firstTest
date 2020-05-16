24a91b38371c300081d7e44bbfe1b57aa18a5e36
Zalim Tsorionov <zalim.tsorion@gmail.com>
2020-05-17 00:29:03 +0300

    kasmedia first commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(function () {
+
+    $('.portfolio-slider').slick();
+
+    $('.feedback-slider').slick();
+
+
+    $('.menu-toggle1').click(function (e) {
+        e.preventDefault();
+        $('.menu-mobile1').toggleClass('d-flex');
+
+    })
+
+    $('.menu-toggle2').click(function (e) {
+        e.preventDefault();
+        $('.menu-mobile2').toggleClass('d-flex');
+
+    })
+
+    $('.menu-toggle3').click(function (e) {
+        e.preventDefault();
+        $('.menu-mobile3').toggleClass('d-flex');
+
+    })
+
+    $('.menu-toggle4').click(function (e) {
+        e.preventDefault();
+        $('.menu-mobile4').toggleClass('d-flex');
+
+    })
+
+    $('.menu-toggle5').click(function (e) {
+        e.preventDefault();
+        $('.menu-mobile5').toggleClass('d-flex');
+
+    })
+
+
+
+});
+
+
+
+
+
+
+
